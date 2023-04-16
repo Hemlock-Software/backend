99856a4d58893dc941da5fd49e7f34dabf97d0fa
MrYao <3052791719@qq.com>
2023-04-16 15:27:57 +0800

    修改了请求返回的结构体

--- src/main/java/com/hemlock/www/backend/controller/UserController.java
+++ src/main/java/com/hemlock/www/backend/controller/UserController.java
@@ -12,6 +12,8 @@ import com.hemlock.www.backend.request.MailArgs;
 import com.hemlock.www.backend.user.*;
 import org.apache.commons.mail.EmailException;
 import org.apache.commons.mail.HtmlEmail;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.security.SecureRandom;
@@ -22,7 +24,7 @@ import java.util.Objects;
 
 import java.util.Random;
 
-@CrossOrigin
+@CrossOrigin(methods={RequestMethod.POST})
 @RestController
 @RequestMapping("/user")
 public class UserController {
@@ -43,14 +45,16 @@ public class UserController {
      * @param token 用于注册认证的token，获取验证码时返回
      */
     @RequestMapping(value = "/join", method = RequestMethod.POST, produces = "application/json;charset=UTF-8")
-    public JSONResult<String> Join(@RequestBody JoinArgs args, @RequestHeader("Authorization") String token) throws EmailException {
+    public ResponseEntity<String> Join(@RequestBody JoinArgs args, @RequestHeader("Authorization") String token) throws EmailException {
         if (args.getMail() == null || args.getIsManager() == null || args.getPassword() == null) {
-            return new JSONResult<String>("400", "Missing field", "");
+//            return new JSONResult<String>("400", "Missing field", "");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Missing field!");
         }
 
         Long storedKeyNum = BackendApplication.ColdData.Exists(args.getMail());
         if (storedKeyNum > 0) {
-            return new JSONResult<String>("400", "This mail has been used!", "");
+//            return new JSONResult<String>("400", "This mail has been used!", "");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("This mail has been used!");
         }
 
         UserValue userValue = new UserValue(args.getMail(), args.getPassword(), args.getIsManager());
@@ -58,11 +62,13 @@ public class UserController {
             userValue.setNickname(args.getNickname());
         }
         if (token.length() < 7) {
-            return new JSONResult<String>("400", "未携带验证码!", "");
+//            return new JSONResult<String>("400", "未携带验证码!", "");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("No verification code is carried!");
         }
         String realToken = BackendApplication.TokenServer.Verify(token.substring(7));
         if (realToken == null) {
-            return new JSONResult<String>("400", "验证码过期", "");
+//            return new JSONResult<String>("400", "验证码过期", "");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Verification code expired!");
         }
         TokenData tokenData = JSON.parseObject(realToken, TokenData.class);
 //        System.out.println(tokenData.getVerifyCode());
@@ -70,15 +76,17 @@ public class UserController {
 //        System.out.println(args.getMail());
 //        System.out.println(args.getVerifyCode());
         if (!Objects.equals(args.getVerifyCode(), tokenData.getVerifyCode()) || !Objects.equals(args.getMail(), tokenData.getEmail())) {
-            return new JSONResult<String>("400", "验证码错误", "");
+//            return new JSONResult<String>("400", "验证码错误", "");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Verification code error!");
         }
         String storedUserValue = JSON.toJSONString(userValue);
 
         if (BackendApplication.ColdData.Set(args.getMail(), storedUserValue))
-            return new JSONResult<String>("200", "success", "");
-
+//            return new JSONResult<String>("200", "success", "");
+            return ResponseEntity.status(HttpStatus.OK).body("success!");
         else
-            return new JSONResult<String>("400", "fail", "");
+//            return new JSONResult<String>("400", "fail", "");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("DB error!");
 
     }
 
@@ -88,16 +96,18 @@ public class UserController {
      * @param args 登录请求体参数
      */
     @RequestMapping(value = "/login", method = RequestMethod.POST, produces = "application/json;charset=UTF-8")
-    public JSONResult<LoginReply> Login(@RequestBody LoginArgs args) {
+    public ResponseEntity<String> Login(@RequestBody LoginArgs args) {
         LoginReply reply = new LoginReply();
 
         if (args.getMail() == null || args.getPassword() == null) {
-            return new JSONResult<LoginReply>("400", "Missing field", reply);
+//            return new JSONResult<LoginReply>("400", "Missing field", reply);
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Missing field!");
         }
 
         Long storedKeyNum = BackendApplication.ColdData.Exists(args.getMail());
         if (storedKeyNum == 0) {
-            return new JSONResult<LoginReply>("400", "No such mail!", reply);
+//            return new JSONResult<LoginReply>("400", "No such mail!", reply);
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("This mail has been used!");
         }
 
         String storedUserJson = BackendApplication.ColdData.Get(args.getMail());
@@ -107,9 +117,10 @@ public class UserController {
         if (Objects.equals(args.getPassword(), storedUserValue.getPassword())) {
             TokenData tokenData = new TokenData(args.getMail(), null, TokenData.Type.Login);
             reply.setToken(BackendApplication.TokenServer.SetToken(tokenData));
-            return new JSONResult<LoginReply>("200", "success", reply);
+            return ResponseEntity.status(HttpStatus.OK).body(BackendApplication.TokenServer.SetToken(tokenData));
+//            return new JSONResult<LoginReply>("200", "success", reply);
         } else {
-            return new JSONResult<LoginReply>("400", "Wrong password!", reply);
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Password error!");
         }
     }
 
@@ -140,7 +151,7 @@ public class UserController {
      * @throws EmailException 发送邮件的异常
      */
     @RequestMapping(value = "/sendMail", method = RequestMethod.POST, produces = "application/json;charset=UTF-8")
-    public JSONResult<String> sendVerificationCode(@RequestBody MailArgs args) throws EmailException {
+    public ResponseEntity<String> sendVerificationCode(@RequestBody MailArgs args) throws EmailException {
 //        Long storedKeyNum = BackendApplication.ColdData.Exists(args.getMail());
 //        if (storedKeyNum > 0) {
 //            return new JSONResult<String>("400", "This mail has been used!", "");
@@ -179,10 +190,12 @@ public class UserController {
             //将email和验证码放入token data，并转化为字符串，生成带有这两个变量的token
             TokenData tokenData = new TokenData(args.getMail(), uid.toString(), TokenData.Type.Register);
             String token = BackendApplication.TokenServer.SetToken(tokenData);   //10分钟过期
-            return new JSONResult<String>("200", "success", token);
+//            return new JSONResult<String>("200", "success", token);
+            return ResponseEntity.status(HttpStatus.OK).body(token);
 
         } else
-            return new JSONResult<String>("400", "fail", "");
+//            return new JSONResult<String>("400", "fail", "");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("DB error!");
 
 
     }
@@ -193,27 +206,32 @@ public class UserController {
      * @param args 找回密码的参数
      */
     @RequestMapping(value = "/findPassword", method = RequestMethod.POST, produces = "application/json;charset=UTF-8")
-    public JSONResult<String> findPassword(@RequestBody FindPasswordArgs args,@RequestHeader("Authorization") String token) {
+    public ResponseEntity<String> findPassword(@RequestBody FindPasswordArgs args,@RequestHeader("Authorization") String token) {
         Long storedKeyNum = BackendApplication.ColdData.Exists(args.getMail());
         if (storedKeyNum == 0) {
-            return new JSONResult<String>("400", "This mail has never been used!", "");
+//            return new JSONResult<String>("400", "This mail has never been used!", "");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("This mail has never been used!");
         }
 
         if (args.getMail() == null || args.getPassword() == null) {
-            return new JSONResult<String>("400", "Missing field", "");
+//            return new JSONResult<String>("400", "Missing field", "");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Missing Field!");
         }
 
         if (token.length() < 7) {
-            return new JSONResult<String>("400", "未携带验证码!", "");
+//            return new JSONResult<String>("400", "未携带验证码!", "");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("No verification code is carried!");
         }
 
         String realToken = BackendApplication.TokenServer.Verify(token.substring(7));
         if (realToken == null) {
-            return new JSONResult<String>("400", "验证码过期", "");
+//            return new JSONResult<String>("400", "验证码过期", "");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Verification code expired");
         }
         TokenData tokenData = JSON.parseObject(realToken, TokenData.class);
         if (!Objects.equals(args.getVerifyCode(), tokenData.getVerifyCode()) || !Objects.equals(args.getMail(), tokenData.getEmail())) {
-            return new JSONResult<String>("400", "验证码错误", "");
+//            return new JSONResult<String>("400", "验证码错误", "");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Verification code error!");
         }
 
         String storedUserJson = BackendApplication.ColdData.Get(args.getMail());
@@ -223,10 +241,12 @@ public class UserController {
         String newUserValue = JSON.toJSONString(storedUserValue);
 
         if (BackendApplication.ColdData.Set(args.getMail(), newUserValue))
-            return new JSONResult<String>("200", "success", "");
+//            return new JSONResult<String>("200", "success", "");
+            return ResponseEntity.status(HttpStatus.OK).body("success!");
 
         else
-            return new JSONResult<String>("400", "fail", "");
+//            return new JSONResult<String>("400", "fail", "");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("DB error!");
     }
 }
 
