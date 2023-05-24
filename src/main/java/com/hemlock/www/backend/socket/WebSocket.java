2aa5fc4807f8b24fc4ce41fb5d7b4078a5f648d1
gdz <13954899675@163.com>
2023-05-24 19:03:52 +0800

    socket

--- src/main/java/com/hemlock/www/backend/socket/WebSocket.java
+++ src/main/java/com/hemlock/www/backend/socket/WebSocket.java
@@ -1,13 +1,17 @@
 package com.hemlock.www.backend.socket;
 
-import com.hemlock.www.backend.room.Member;
-import com.hemlock.www.backend.room.Message;
+import com.alibaba.fastjson.JSON;
+import com.hemlock.www.backend.BackendApplication;
+import com.hemlock.www.backend.room.*;
+import com.hemlock.www.backend.user.UserValue;
+import com.sun.mail.imap.MessageVanishedEvent;
 import jakarta.websocket.*;
 import jakarta.websocket.server.PathParam;
 import jakarta.websocket.server.ServerEndpoint;
 import org.springframework.stereotype.Component;
 
 import java.sql.Time;
+import java.text.SimpleDateFormat;
 import java.time.Instant;
 import java.util.Date;
 import java.util.Map;
@@ -33,6 +37,7 @@ public class WebSocket {
         //连接成功
         onlineClientNumber.incrementAndGet();//在线数+1
         onlineClientMap.put(session.getId(),session);//添加当前连接的session
+        System.out.println("connect: "+onlineClientNumber);
 
     }
 
@@ -41,6 +46,7 @@ public class WebSocket {
         //连接关闭
         onlineClientNumber.decrementAndGet();//在线数-1
         onlineClientMap.remove(session.getId());//移除当前连接的session
+        System.out.println("close: "+onlineClientNumber);
     }
 
     @OnError
@@ -52,28 +58,35 @@ public class WebSocket {
     @OnMessage
     public void onMessage(Session session, String content, @PathParam("roomid") String roomid, @PathParam("username") String username){
         System.out.println("roomid:"+roomid+" username:"+username);
+        System.out.println("message1: "+onlineClientNumber);
 
-        //收到消息
-        Message message = new Message();
+        String storedUserJson = BackendApplication.ColdData.Get(username);
+        UserValue storedUserValue = JSON.parseObject(storedUserJson, UserValue.class);
 
-        message.setRoomID(roomid);
-        //messageID怎么获得
-        message.setMessageID("");
-        message.setContent(content);
-        message.setTime(new Time(new Date().getTime()));
-        Member member = new Member();
-        //member的email nickname怎么获得
-        message.setSender(member);
+        Member owner = new Member(username, storedUserValue.getNickname());
 
-        //message插入数据库
+        MessageKey key = new MessageKey();
+        key.setMessageID(Integer.parseInt(RoomHot.incrementLastMessageID(roomid)));
+        key.setRoomID(roomid);
+
+        Date date = new Date();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd :hh:mm:ss");
+
+        MessageValue newMsg = new MessageValue();
+        newMsg.setContent(content);
+        newMsg.setTime(dateFormat.format(date));
+        newMsg.setSender(owner);
+
+        BackendApplication.HotData.Set(JSON.toJSONString(key), JSON.toJSONString(newMsg));
+
 
         //转发给其他用户
         Set<String> sessionIdSet = onlineClientMap.keySet(); //获得Map的Key的集合
         for (String sessionId : sessionIdSet) { //迭代Key集合
             Session session1 = onlineClientMap.get(sessionId); //根据Key得到value
-            session.getAsyncRemote().sendText(content); //发送消息给客户端
+            session1.getAsyncRemote().sendText(JSON.toJSONString(newMsg)); //发送消息给客户端
         }
-
+        System.out.println("message2: "+onlineClientNumber);
     }
 
 
